60d58e87e58bf76c95b6d1839a2d8fa03d1520a2
kcruz <kaysabelle@gmail.com>
2015-03-16 14:57:44 -0700

    Fix image sources

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 function low() {
-	$('#smokey').attr('src', '/img/low.png');
+	$('#smokey').attr('src', './img/low.png');
 }
 
 function moderate() {
-	$('#smokey').attr('src', '/img/moderate.png');
+	$('#smokey').attr('src', './img/moderate.png');
 }
 
 function high() {
-	$('#smokey').attr('src', '/img/high.png');
+	$('#smokey').attr('src', './img/high.png');
 }
 
 function veryhigh() {
-	$('#smokey').attr('src', '/img/veryhigh.png');
+	$('#smokey').attr('src', './img/veryhigh.png');
 }
 
 function extreme() {
-	$('#smokey').attr('src', '/img/extreme.png');
+	$('#smokey').attr('src', './img/extreme.png');
 }
