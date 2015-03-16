4ed3b6a4e337bbdbde37fbee2b2b2dc703a2c9e6
kcruz <kaysabelle@gmail.com>
2015-03-16 14:52:35 -0700

    Add buttons to toggle image of Smokey

---
+++ index.js
@@ -0,0 +1,19 @@
+function low() {
+	$('#smokey').attr('src', '/img/low.png');
+}
+
+function moderate() {
+	$('#smokey').attr('src', '/img/moderate.png');
+}
+
+function high() {
+	$('#smokey').attr('src', '/img/high.png');
+}
+
+function veryhigh() {
+	$('#smokey').attr('src', '/img/veryhigh.png');
+}
+
+function extreme() {
+	$('#smokey').attr('src', '/img/extreme.png');
+}
